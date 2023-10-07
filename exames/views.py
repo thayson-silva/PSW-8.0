2bc61c6780c6b13fbde373c98d30a67654b9fee0
thayson <thayson.silva@hotmail.com>
2023-10-07 20:01:57 -0300

    Acesso Médico

--- exames/views.py
+++ exames/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
-from .models import TiposExames, SolicitacaoExame, PedidosExames
+from .models import TiposExames, SolicitacaoExame, PedidosExames, AcessoMedico
 from django.http import HttpResponse
 from datetime import datetime
 from django.contrib.messages import constants
@@ -75,6 +75,11 @@ def gerenciar_exames(request):
 @login_required
 def permitir_abrir_exame(request, exame_id):
     exame = SolicitacaoExame.objects.get(id = exame_id)
+
+    if not exame.resultado:
+        messages.add_message(request, constants.ERROR, 'O exame não possui um resultado!')
+        return redirect('/exames/gerenciar_exames/')
+
     if not exame.requer_senha:
         return redirect(exame.resultado.url)
     
@@ -90,12 +95,52 @@ def solicitar_senha_exame(request, exame_id):
         senha_informada = request.POST.get('senha')
 
         if senha_informada == exame.senha:
+
             return redirect(exame.resultado.url)
         
         messages.add_message(request, constants.ERROR, 'Senha incorreta!')
         return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
 
+@login_required
+def gerar_acesso_medico(request):
+    if request.method == "GET":
+        acessos_medicos = AcessoMedico.objects.filter(usuario =request. user)
+        return render(request, 'gerar_acesso_medico.html', {'acessos_medicos': acessos_medicos})
+    elif request.method == "POST":
+        identificacao = request.POST.get('identificacao')
+        tempo_de_acesso = request.POST.get('tempo_de_acesso')
+        data_exame_inicial = request.POST.get("data_exame_inicial")
+        data_exame_final = request.POST.get("data_exame_final")
+
+        acesso_medico = AcessoMedico(
+            usuario = request.user,
+            identificacao = identificacao,
+            tempo_de_acesso = tempo_de_acesso,
+            data_exames_iniciais = data_exame_inicial,
+            data_exames_finais = data_exame_final,
+            criado_em = datetime.now()
+        )
+
+        acesso_medico.save()
+
+        messages.add_message(request, constants.SUCCESS, 'Acesso gerado com sucesso')
+        return redirect('/exames/gerar_acesso_medico')
+    
+def acesso_medico(request, token):
+    acesso_medico = AcessoMedico.objects.get(token = token)
+
+    if acesso_medico.status == 'Expirado':
+        messages.add_message(request, constants.WARNING, 'Esse link já se expirou!')
+        return redirect('/usuarios/login')
+
+    pedidos = PedidosExames.objects.filter(data__gte = acesso_medico.data_exames_iniciais).filter(data__lte = acesso_medico.data_exames_finais).filter(usuario=acesso_medico.usuario)
+
+    return render(request, 'acesso_medico.html', {'pedidos': pedidos})
+
+
+
+
 #para testar o código
 # print("")
-# print(pedido_id)
+# print(exame.resultado)
 # print("")
