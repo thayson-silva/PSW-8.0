4286b94e35a6d18377f47a146514e58a76a4b01b
thayson <thayson.silva@hotmail.com>
2023-10-07 18:09:49 -0300

    aula 2

--- exames/models.py
+++ exames/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import User
+from django.db import models
+from django.utils.safestring import mark_safe
 
 class TiposExames(models.Model):
     tipo_choices = (
@@ -15,7 +16,8 @@ class TiposExames(models.Model):
 
     def __str__(self):
         return self.nome
-    
+
+
 class SolicitacaoExame(models.Model):
     choice_status = (
         ('E', 'Em análise'),
@@ -23,13 +25,23 @@ class SolicitacaoExame(models.Model):
     )
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
     exame = models.ForeignKey(TiposExames, on_delete=models.DO_NOTHING)
-    status = models.CharField(max_length=2, choices=choice_status)
+    status = models.CharField(max_length=2, choices=choice_status, default='E')
     resultado = models.FileField(upload_to="resultados", null=True, blank=True)
     requer_senha = models.BooleanField(default=False)
     senha = models.CharField(max_length=6, null=True, blank=True)
 
     def __str__(self):
         return f'{self.usuario} | {self.exame.nome}'
+    
+    def badge_template(self):
+        if self.status == 'E':
+            classes_css = 'bg-warning text-dark'
+            texto = "Em análise"
+        elif self.status == 'F':
+            classes_css = 'bg-success'
+            texto = "Finalizado"
+        return mark_safe(f"<span class='badge bg-primary {classes_css}'>{texto}</span>")
+
 
 class PedidosExames(models.Model):
     usuario = models.ForeignKey(User, on_delete=models.DO_NOTHING)
--- exames/views.py
+++ exames/views.py
@@ -1,3 +1,101 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import TiposExames, SolicitacaoExame, PedidosExames
+from django.http import HttpResponse
+from datetime import datetime
+from django.contrib.messages import constants
+from django.contrib import messages
 
-# Create your views here.
+@login_required #se não estiver com usuario na sessão ele da um 404
+def solicitar_exames(request):
+    context = {
+        'tipos_exames' : TiposExames.objects.all(),
+    }
+
+    if request.method == 'GET':
+        return render(request, 'solicitar_exames.html', context=context)
+    elif request.method == 'POST':
+        exames_id = request.POST.getlist('exames')
+        exames_solicitados = TiposExames.objects.filter(pk__in = exames_id)
+
+        total = 0
+        for i in exames_solicitados:
+            if i.disponivel:
+                total += i.preco        
+
+        context['exames_solicitados'] = exames_solicitados
+        context['total'] = total
+        return render(request, 'solicitar_exames.html', context=context)
+
+@login_required        
+def fechar_pedido(request):
+    exames_id = request.POST.getlist('exames')  
+    solicitacao_exames  = TiposExames.objects.filter(pk__in = exames_id)
+    
+    pedido_exame = PedidosExames(
+        usuario = request.user,
+        data = datetime.now()
+    )
+    pedido_exame.save()
+    
+    for exame in solicitacao_exames :
+        exame_solicitado = SolicitacaoExame(
+            usuario = request.user,
+            exame = exame
+        )
+        exame_solicitado.save()
+        pedido_exame.exames.add(exame_solicitado)
+
+    messages.add_message(request, constants.SUCCESS, 'Pedido de exame concluído com sucesso')
+    return redirect('/exames/gerenciar_pedidos/')
+
+@login_required
+def gerenciar_pedidos(request):
+    pedidos_exames = PedidosExames.objects.filter(usuario=request.user)
+    return render(request, 'gerenciar_pedidos.html', {'pedidos_exames': pedidos_exames})
+
+@login_required
+def cancelar_pedido(request, pedido_id):
+    pedido = PedidosExames.objects.get(id = pedido_id)
+
+    if request.user != pedido.usuario:
+        messages.add_message(request, constants.ERROR, 'Não é possivel cancelar um pedido que não é seu!')
+        return redirect('/exames/gerenciar_pedidos/')
+        
+    pedido.agendado = False
+    pedido.save()
+    messages.add_message(request, constants.SUCCESS, 'Pedido cancelado com sucesso!')
+    return redirect('/exames/gerenciar_pedidos/')
+
+@login_required
+def gerenciar_exames(request):
+    exames = SolicitacaoExame.objects.filter(usuario = request.user)
+    return render(request, 'gerenciar_exames.html', {'exames' : exames})
+
+@login_required
+def permitir_abrir_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(id = exame_id)
+    if not exame.requer_senha:
+        return redirect(exame.resultado.url)
+    
+    return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
+
+@login_required
+def solicitar_senha_exame(request, exame_id):
+    exame = SolicitacaoExame.objects.get(pk = exame_id)
+
+    if request.method == 'GET':
+        return render(request, 'solicitar_senha_exame.html', {'exame': exame})
+    elif request.method == 'POST':
+        senha_informada = request.POST.get('senha')
+
+        if senha_informada == exame.senha:
+            return redirect(exame.resultado.url)
+        
+        messages.add_message(request, constants.ERROR, 'Senha incorreta!')
+        return redirect(f'/exames/solicitar_senha_exame/{exame.id}')
+
+#para testar o código
+# print("")
+# print(pedido_id)
+# print("")
